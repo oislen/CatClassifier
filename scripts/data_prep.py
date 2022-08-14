bfadbf72d62aea152babcd5eab53f95c5e16f7cd
Oisin <oisin.leonard@gmail.com>
2022-08-14 21:57:42 +0100

    #1 output processed output data as .pickle file

--- scripts/data_prep.py
+++ scripts/data_prep.py
@@ -12,8 +12,9 @@ from utilities.greyscale_image import greyscale_image
 from utilities.pad_image import pad_image
 
 # set image file directories
-cat_image_fdir = os.path.join(root_fdir, 'cats')
-dog_image_fdir = os.path.join(root_fdir, 'dogs')
+data_fdir = os.path.join(root_fdir, 'data')
+cat_image_fdir = os.path.join(data_fdir, 'cats')
+dog_image_fdir = os.path.join(data_fdir, 'dogs')
 
 # create image file paths and classes
 cat_image_fpaths = {os.path.join(cat_image_fdir, cat_image_fname):'cat' for cat_image_fname in os.listdir(cat_image_fdir)}
@@ -50,5 +51,5 @@ image_dataframe['pad_image_array'] = image_dataframe['image_array'].apply(lambda
 # subset the output image data
 output = image_dataframe[['image_fpath', 'pad_image_array', 'target']]
 
-# plot image
-plot_image(output.loc[1, 'pad_image_array'])
+# save processed data as a pickle file
+output.to_pickle(os.path.join(data_fdir, 'model_data.pickle'))
