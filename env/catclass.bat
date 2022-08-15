14fc6c8213ff852890ba7de0cb260a26ce1a6908
Oisin <oisin.leonard@gmail.com>
2022-08-15 10:18:58 +0100

    #3 Added sklearn, tensorflow and keras

--- env/catclass.bat
+++ env/catclass.bat
@@ -6,19 +6,24 @@ call conda activate catclass
 :: update conda version
 call conda update -n base conda --yes
 
-:: conda install required webscraping packages
+:: install required webscraping packages
 call pip install Pillow
 call pip install playwright
 call conda install -c conda-forge requests --yes
 call conda install -c conda-forge beautifulsoup4 --yes
 call conda install -c conda-forge scrapy --yes
 
-:: conda install required data processing packages
+:: install required data processing packages
 call conda install -c conda-forge numpy --yes
 call conda install -c conda-forge pandas --yes
 call pip install opencv-python
 
-:: conda install equired visualisation packages
+:: install required model 
+call pip install scikit-learn
+call pip install tensorflow-cpu
+call pip install keras
+
+:: install required visualisation packages
 call conda install -c conda-forge matplotlib --yes
 call conda install -c conda-forge seaborn --yes
 
