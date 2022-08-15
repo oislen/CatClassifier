cf3ab694095c314b12390c3f0b7901ffa2bcacfc
Oisin <oisin.leonard@gmail.com>
2022-08-15 10:19:22 +0100

    #3 added file directory paths to scripts, utilities, keras and data

--- scripts/cons.py
+++ scripts/cons.py
@@ -1,1 +1,11 @@
+import os
+import sys
+
 root_fdir = 'E:\\GitHub\\cat_classifier'
+data_fdir = os.path.join(root_fdir, 'data')
+scripts_fdir = os.path.join(root_fdir, 'scripts')
+utilities_fdir = os.path.join(scripts_fdir, 'utilities')
+keras_fdir = os.path.join(scripts_fdir, 'keras')
+
+for fdir in [root_fdir, data_fdir, scripts_fdir, utilities_fdir, keras_fdir]:
+    sys.path.append(fdir)
