d73bdf96050334171846106d2d2bc688361e87b4
Oisin <oisin.leonard@gmail.com>
2022-08-15 20:54:52 +0100

    #3 added keep3dim parameter to maintain 3rd dimensions of numpy arrays

--- scripts/utilities/greyscale_image.py
+++ scripts/utilities/greyscale_image.py
@@ -1,9 +1,12 @@
 import numpy as np
 
-def greyscale_image(rgb_image_array):
+def greyscale_image(rgb_image_array, keep_3dim = True):
     """"""
     # apply grey scale transformation
     grey_image_array =  np.dot(rgb_image_array[:, :, :3], [0.2125, 0.7154, 0.0721])
     # floor transformed pixel floats
     gray_img = grey_image_array.astype(np.uint8)
+    # if keeping third dimension
+    if keep_3dim:
+        gray_img = gray_img[..., np.newaxis]
     return gray_img
--- scripts/utilities/resize_image.py
+++ scripts/utilities/resize_image.py
@@ -1,7 +1,12 @@
+import numpy as np
 import cv2
 
-def resize_image(image_array, reshape_wh, interpolation = cv2.INTER_LINEAR):
+def resize_image(image_array, reshape_wh, interpolation = cv2.INTER_LINEAR, keep_3dim = True):
     """"""
     # rescale the image either by shrinking or expanding
     res_image_array = cv2.resize(image_array, dsize = reshape_wh, interpolation=interpolation)
+    # keep 3dim; when applying resizing to (:, :, 1) shape images
+    if keep_3dim and len(res_image_array.shape) == 2:
+        res_image_array = res_image_array[..., np.newaxis]
+
     return res_image_array
