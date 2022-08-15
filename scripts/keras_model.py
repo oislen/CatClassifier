57ec65ab60d7ca53bee5dd03dba4cc6cf39413b8
Oisin <oisin.leonard@gmail.com>
2022-08-15 20:53:55 +0100

    #3 Got lenet model working

--- scripts/keras_model.py
+++ scripts/keras_model.py
@@ -3,13 +3,15 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from keras.optimizers import RMSprop
+from keras.callbacks import ReduceLROnPlateau
 
 import cons
 from utilities.plot_image import plot_image
 from LeNet_Model import LeNet_Model
 from fit_model import fit_model
+from plot_model import plot_model_fit
 
-
+# set data file directory
 data_fdir = cons.data_fdir
 
 # load in processed data
@@ -18,36 +20,48 @@ data = pd.read_pickle(os.path.join(data_fdir, 'model_data.pickle'))
 # randomly shuffle data
 data = data.sample(frac = 1.0, replace = False)
 
-data.head()
+# extract out X dataframe and y series
+y_train = data['target'].values
+X_train = np.stack(data['pad_image_array'].values)
 
-y_train = data['target']
-X_train = data['pad_image_array']
-
+# standardise values between 0 and 1
 X_train = X_train / 255
 
-# reshape image into 3 dimensions: height = 28px, width = 28px, canal = 1 (greyscale)
-X_train[0][:, : np.newaxis].shape
-X_train = X_train.values.reshape(512, 256, 1)
-
-a = X_train[0]
-a.shape
-a_new = a[..., np.newaxis]
-a_new[:, :, 0]
-a[..., np.newaxis].shape
-a[:, : 1] = [1]
-X_train[0][:, : 1].shape
-
+# split data into training and validation sets
 X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size = 0.1)
 
-X_train.head()
-y_train.head()
+# initiate lenet model
+lenet_model = LeNet_Model(image_shape = X_train[0].shape, n_targets = 1)
 
-plot_image(X_train.loc[0, 'pad_image_array'])
-
-lenet_model = LeNet_Model(image_shape = X_train[0].shape, n_targets = 2)
-
+# intiate rms prop optimiser
 optimizer = RMSprop(learning_rate = 0.001, 
                     rho = 0.9, 
                     epsilon = 1e-08, 
                     decay = 0.0
                     )
+
+# set a learning rate annealer
+learning_rate_reduction = ReduceLROnPlateau(monitor = 'accuracy', 
+                                            patience = 3, 
+                                            verbose = 1, 
+                                            factor = 0.5, 
+                                            min_lr = 0.00001
+                                            )
+
+# Attention: Windows implementation may cause an error here. In that case use model_name=None.
+model_fit = fit_model(model = lenet_model, 
+                     epochs = 10,
+                     loss = 'binary_crossentropy',
+                     starting_epoch = None,
+                     batch_size = 32,
+                     optimizer = optimizer,
+                     lrate_red = learning_rate_reduction,
+                     datagen = None, 
+                     X_train = X_train,
+                     X_val = X_valid, 
+                     Y_train = y_train, 
+                     Y_val = y_valid
+                     )
+
+# plot model fits
+plot_model_fit(model_fit = model_fit)
