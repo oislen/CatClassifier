e320cd0836101b1f1bafd678bb096f326fffb904
Oisin <oisin.leonard@gmail.com>
2022-08-16 14:02:18 +0100

    #6 Created script to download cats-vs-dogs data from kaggle

--- scripts/cons.py
+++ scripts/cons.py
@@ -6,6 +6,13 @@ data_fdir = os.path.join(root_fdir, 'data')
 scripts_fdir = os.path.join(root_fdir, 'scripts')
 utilities_fdir = os.path.join(scripts_fdir, 'utilities')
 keras_fdir = os.path.join(scripts_fdir, 'keras')
+kaggle_fdir = os.path.join(scripts_fdir, 'kaggle')
 
-for fdir in [root_fdir, data_fdir, scripts_fdir, utilities_fdir, keras_fdir]:
+for fdir in [root_fdir, data_fdir, scripts_fdir, utilities_fdir, keras_fdir, kaggle_fdir]:
     sys.path.append(fdir)
+
+# set kaggle competition name
+comp_name = 'dogs-vs-cats'
+download_data = True
+unzip_data = False
+del_zip = False
---
+++ scripts/download_kaggle_data.py
@@ -0,0 +1,11 @@
+# load relevant libraries
+import cons
+from download_comp_data import download_comp_data
+     
+# download competition data
+download_comp_data(comp_name = cons.comp_name,
+                   data_dir = cons.data_fdir,
+                   download_data = cons.download_data, 
+                   unzip_data = cons.unzip_data, 
+                   del_zip = cons.del_zip
+                   )
