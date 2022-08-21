9d2a8d6c728a66a2b8d5a551e17a65d9b97d343c
Oisin <oisin.leonard@gmail.com>
2022-08-21 09:01:49 +0100

    #4 Updated docker tag name

--- exeDocker.bat
+++ exeDocker.bat
@@ -1,6 +1,6 @@
 :: set docker settings
 SET DOCKER_USER=oislen
-SET DOCKER_REPO=catclass
+SET DOCKER_REPO=Cat-Classifier
 SET DOCKER_TAG=latest
 SET DOCKER_IMAGE=%DOCKER_USER%/%DOCKER_REPO%:%DOCKER_TAG%
 
@@ -12,12 +12,12 @@ docker rm -f %DOCKER_IMAGE%
 call docker build --no-cache -t %DOCKER_IMAGE% . 
 ::call docker build -t %DOCKER_IMAGE% .
 
-:: push docker container to DockerHub
-:: call docker push %DOCKER_IMAGE%
-
 :: run docker container
 SET UBUNTU_DIR=/home/ubuntu
 call docker run --shm-size=512m -p 8889:8888 -it %DOCKER_IMAGE%
 :: docker images
 :: docker ps -a
 :: docker exec -it container_id /bin/bash
+
+:: push docker container to DockerHub
+:: call docker push %DOCKER_IMAGE%
