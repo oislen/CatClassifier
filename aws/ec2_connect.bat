75aa22dcf6de3773af5f330cb441e675aefd9829
Oisin <oisin.leonard@gmail.com>
2022-08-21 09:16:01 +0100

    #4 updated ec2 connect with new fodler structure

--- aws/ec2_connect.bat
+++ aws/ec2_connect.bat
@@ -1,8 +1,8 @@
 :: set EC2 login info
 SET EC2_USER=ec2-user
 SET EC2_PEM_FPATH="C:\Users\oisin\.aws\kaggle.pem"
-SET EC2_CREDS_FDIR=E:\GitHub\cat_classifier\.creds
-SET EC2_SETUP_FPATH=E:\GitHub\cat_classifier\environments\linux_docker_setup.sh
+SET EC2_CREDS_FDIR=E:\GitHub\Cat-Classifier\.creds
+SET EC2_SETUP_FPATH=E:\GitHub\Cat-Classifier\aws\linux_docker_setup.sh
 SET EC2_DNS_FPATH=%EC2_CREDS_FDIR%\ec2_dns
 
 :: enable delyaed expansion to allow parsing of the %EC2_DNS_FPATH% file
