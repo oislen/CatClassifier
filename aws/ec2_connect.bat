27b8f02da3b74cb38b31fdf2b88254770aea912b
Oisin <oisin.leonard@gmail.com>
2022-09-04 17:04:25 +0100

    #4 Pushing docker data extract to ec2 instance

--- aws/ec2_connect.bat
+++ aws/ec2_connect.bat
@@ -3,6 +3,7 @@ SET EC2_USER=ec2-user
 SET EC2_PEM_FPATH="C:\Users\oisin\.aws\kaggle.pem"
 SET EC2_CREDS_FDIR=E:\GitHub\Cat-Classifier\.creds
 SET EC2_SETUP_FPATH=E:\GitHub\Cat-Classifier\aws\linux_docker_setup.sh
+SET EC2_EXTRACT_FPATH=E:\GitHub\Cat-Classifier\aws\docker_extract_data.sh
 SET EC2_DNS_FPATH=%EC2_CREDS_FDIR%\ec2_dns
 
 :: enable delyaed expansion to allow parsing of the %EC2_DNS_FPATH% file
@@ -14,6 +15,7 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`type %EC2_DNS_FPATH%`) DO (
 :: scp docker file and linuc setup shell script to EC2 home
 call scp -i %EC2_PEM_FPATH% -r %EC2_CREDS_FDIR% %EC2_USER%@%EC2_DNS%:~/.
 call scp -i %EC2_PEM_FPATH% %EC2_SETUP_FPATH% %EC2_USER%@%EC2_DNS%:~/linux_docker_setup.sh
+call scp -i %EC2_PEM_FPATH% %EC2_EXTRACT_FPATH% %EC2_USER%@%EC2_DNS%:~/docker_extract_data.sh
 :: ssh to EC2
 call ssh -i %EC2_PEM_FPATH% %EC2_USER%@%EC2_DNS%
 ENDLOCAL
