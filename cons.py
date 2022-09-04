26dc6906ca19f830e3b0b394a8d29d4ee0b230df
Oisin <oisin.leonard@gmail.com>
2022-09-04 13:43:31 +0100

    #3  Renamed keras folder to model folder to avoid module import conflicts with keras package

--- cons.py
+++ cons.py
@@ -2,21 +2,21 @@ import os
 import sys
 
 # set root file directories
-#root_fdir = 'E:\\GitHub\\Cat-Classifier'
-root_fdir = '/home/ubuntu/Cat-Classifier'
+root_fdir = 'E:\\GitHub\\Cat-Classifier'
+#root_fdir = '/home/ubuntu/Cat-Classifier'
 data_fdir = os.path.join(root_fdir, 'data')
 dataprep_fdir = os.path.join(root_fdir, 'data_prep')
 env_fdir = os.path.join(root_fdir, 'environments')
 kaggle_fdir = os.path.join(root_fdir, 'kaggle')
-keras_fdir = os.path.join(root_fdir, 'keras')
+model_fdir = os.path.join(root_fdir, 'model')
 report_fdir = os.path.join(root_fdir, 'report')
 test_fdir = os.path.join(data_fdir, 'test1')
 train_fdir = os.path.join(data_fdir, 'train')
 webscrapers_fdir = os.path.join(root_fdir, 'webscrapers')
 
 # set subdirectories
-checkpoints_fdir = os.path.join(keras_fdir, 'checkpoints')
-arch_fdir = os.path.join(keras_fdir, 'arch')
+checkpoints_fdir = os.path.join(model_fdir, 'checkpoints')
+arch_fdir = os.path.join(model_fdir, 'arch')
 
 # set file paths
 train_data_pickle_fpath = os.path.join(data_fdir, 'train_data.pickle')
@@ -26,7 +26,7 @@ keras_model_pickle_fpath = os.path.join(report_fdir, 'keras_model.h5')
 test_preds_pickle_fpath = os.path.join(report_fdir, 'test_preds.pickle')
 
 # set list containing all required directories
-root_fdirs = [root_fdir, data_fdir,  dataprep_fdir, env_fdir, kaggle_fdir, keras_fdir, report_fdir, test_fdir, train_fdir, webscrapers_fdir]
+root_fdirs = [root_fdir, data_fdir,  dataprep_fdir, env_fdir, kaggle_fdir, model_fdir, report_fdir, test_fdir, train_fdir, webscrapers_fdir]
 sub_fdirs = [checkpoints_fdir, arch_fdir]
 
 # append directories to path
