8d7036f04c08a085a0db51f814f8f26defee5587
Oisin <oisin.leonard@gmail.com>
2022-09-30 13:53:17 +0100

    Dynamically extracting container id and added report files to be extracted from container

--- aws/docker_extract_data.sh
+++ aws/docker_extract_data.sh
@@ -1,10 +1,12 @@
+# set container id
+containerId=`docker ps | grep oislen/cat-classifier:latest | awk '{print $1;}'`
 # extract data from docker container
-containerId=containerId
 docker cp ${containerId}:/home/ubuntu/Cat-Classifier/data/keras_model.h5 ~/keras_model.h5
 docker cp ${containerId}:/home/ubuntu/Cat-Classifier/data/model_fit.pickle ~/model_fit.pickle
-docker cp ${containerId}:/home/ubuntu/Cat-Classifier/data/submissions.csv ~/submissions.csv
-
-# scp data to local windows
-scp ~/keras_model.h5 E:\GitHub\Cat-Classifier\data\keras_model.h5
-scp ~/model_fit.pickle E:\GitHub\Cat-Classifier\data\model_fit.pickle
-scp ~/submissions.csv E:\GitHub\Cat-Classifier\data\submissions.csv
+docker cp ${containerId}:/home/ubuntu/Cat-Classifier/data/submission.csv ~/submission.csv
+# extract report from docker container
+docker cp ${containerId}:/home/ubuntu/Cat-Classifier/report/generator_plot.jpg ~/generator_plot.jpg
+docker cp ${containerId}:/home/ubuntu/Cat-Classifier/report/model_accuracy.png ~/model_accuracy.png
+docker cp ${containerId}:/home/ubuntu/Cat-Classifier/report/model_loss.png ~/model_loss.png
+docker cp ${containerId}:/home/ubuntu/Cat-Classifier/report/pred_images.jpg ~/pred_images.jpg
+docker cp ${containerId}:/home/ubuntu/Cat-Classifier/report/random_image.jpg ~/random_image.jpg
