473386f1e0dbcfbc9326e054f95d17547fe0ae09
Oisin <oisin.leonard@gmail.com>
2023-03-12 18:07:00 +0000

    Added useful docker commands to .bat script

--- exeDocker.bat
+++ exeDocker.bat
@@ -15,9 +15,13 @@ call docker build --no-cache -t %DOCKER_IMAGE% .
 :: run docker container
 SET UBUNTU_DIR=/home/ubuntu
 call docker run --shm-size=512m -p 8889:8888 -it %DOCKER_IMAGE%
+
+:: useful docker commands
 :: docker images
 :: docker ps -a
-:: docker exec -it container_id /bin/bash
-
-:: push docker container to DockerHub
-:: call docker push %DOCKER_IMAGE%
+:: docker exec -it {container_hash} /bin/bash
+:: docker stop {container_hash}
+:: docker start -ai {container_hash}
+:: docker rm {container_hash}
+:: docker image rm {docker_image}
+:: docker push {docker_image}
