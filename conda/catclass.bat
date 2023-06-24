f214ccce51450d93d5add96a043088ee7202ece6
Oisin <oisin.leonard@gmail.com>
2023-06-24 11:45:19 +0100

    Updated conda env scripts to pip install from requirements.txt

--- conda/catclass.bat
+++ conda/catclass.bat
@@ -1,4 +1,5 @@
 :: create and activate new environment
+call conda deactivate
 call conda env remove --name catclass
 call conda create --name catclass python --yes
 call conda activate catclass
@@ -6,32 +7,11 @@ call conda activate catclass
 :: update conda version
 call conda update -n base conda --yes
 
-:: install required webscraping packages
-call pip install Pillow
-call pip install playwright
-call conda install -c conda-forge requests --yes
-call conda install -c conda-forge beautifulsoup4 --yes
-call conda install -c conda-forge scrapy --yes
-
-:: install required data processing packages
-call conda install -c conda-forge numpy --yes
-call conda install -c conda-forge pandas --yes
-call pip install opencv-python
-
-:: install required model 
-call pip install scikit-learn
-call pip install tensorflow-cpu
-call pip install keras
-
-:: install required visualisation packages
-call conda install -c conda-forge matplotlib --yes
-call conda install -c conda-forge seaborn --yes
+:: install relevant libraries
+call pip install -r ..\requirements.txt
 
 :: run playwright install
 call playwright install
-
-:: install jupyterlab
-call conda install -c conda-forge jupyterlab --yes
 
 :: deactivate new environment
 call conda deactivate
--- conda/catclass.sh
+++ conda/catclass.sh
@@ -1,4 +1,5 @@
 # create and activate new environment
+conda deactivate
 conda env remove --name catclass
 conda create --name catclass python --yes
 conda activate catclass
@@ -6,26 +7,8 @@ conda activate catclass
 # update conda version
 conda update -n base conda --yes
 
-# install required webscraping packages
-pip install Pillow
-pip install playwright
-conda install -c conda-forge requests --yes
-conda install -c conda-forge beautifulsoup4 --yes
-conda install -c conda-forge scrapy --yes
-
-# install required data processing packages
-conda install -c conda-forge numpy --yes
-conda install -c conda-forge pandas --yes
-pip install opencv-python
-
-# install required model 
-pip install scikit-learn
-pip install tensorflow-cpu
-pip install keras
-
-# install required visualisation packages
-conda install -c conda-forge matplotlib --yes
-conda install -c conda-forge seaborn --yes
+# install relevant libraries
+pip install -r ..\requirements.txt
 
 # run playwright install
 playwright install
