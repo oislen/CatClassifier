6ba319a70d9164e8a2355dfcfb858303e666bec4
Oisin <oisin.leonard@gmail.com>
2023-09-16 19:25:09 +0000

    Cleaned up validate function

--- model/torch/validate.py
+++ model/torch/validate.py
@@ -1,14 +1,13 @@
 import torch 
 
-def validate(model, device, validation_loader, criterion):
+def validate(model, device, dataloader, criterion):
     """
-    """
-    valid_loss_list = []
-    valid_acc_list = []  
+    """ 
+    model = model.to(device)
     model.eval()
     with torch.no_grad():
         v_loss, v_corr = 0.0, 0.0
-        for i, (images, labels) in enumerate(validation_loader):
+        for i, (images, labels) in enumerate(dataloader):
             # load images and labels to device
             images = images.to(device)
             labels = labels.to(device)
@@ -19,11 +18,9 @@ def validate(model, device, validation_loader, criterion):
             v_loss += loss.item() * images.size(0)
             v_corr += torch.sum(preds.argmax(1) == labels) 
         # update training loss and accuarcy
-        valid_loss = v_loss / len(validation_loader.dataset)
-        valid_acc = v_corr.cpu().numpy() / len(validation_loader.dataset)
-        valid_loss_list.append(valid_loss)
-        valid_acc_list.append(valid_acc)  
-        print(f'Valid Loss: {loss.item():.4f} Accuracy: {valid_acc:.4f}%')
-    return valid_loss_list, valid_acc_list
+        valid_loss = v_loss / len(dataloader.dataset)
+        valid_acc = v_corr.cpu().numpy() / len(dataloader.dataset) * 100
+        print(f'Valid Loss: {loss.item():.4f}, Valid Accuracy: {valid_acc:.4f}%')
+    return valid_loss, valid_acc
 
 
