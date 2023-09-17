9b9f6ce4752c2a8ca66b8a41be184cd884c43aaf
Oisin <oisin.leonard@gmail.com>
2023-09-17 13:22:22 +0000

    Adapted function to work with both keras and torch data generators

--- data_prep/utilities/plot_generator.py
+++ data_prep/utilities/plot_generator.py
@@ -1,15 +1,20 @@
 import matplotlib.pyplot as plt
 
-def plot_generator(generator, output_fpath = None):
+def plot_generator(generator, mode = 'keras', output_fpath = None):
     """"""
     # plot example
     plt.figure(figsize=(12, 12))
     for i in range(0, 15):
         plt.subplot(5, 3, i+1)
-        for X_batch, Y_batch in generator:
+        if mode == 'keras':
+            for X_batch, Y_batch in generator:
+                image = X_batch[0]
+                plt.imshow(image)
+                break
+        elif mode == 'torch':
+            X_batch, Y_batch = generator[i]
             image = X_batch[0]
             plt.imshow(image)
-            break
     plt.tight_layout()
     #  show save, plot and close
     if output_fpath != None:
