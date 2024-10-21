a73ec2969db3d0a8791ad1afdc5d56f4476569cc
Oisin <oisin.leonard@gmail.com>
2024-10-21 20:51:22 +0100

    Set up logging

--- 02_prg_scrape_imgs.py
+++ 02_prg_scrape_imgs.py
@@ -1,12 +1,17 @@
+import logging
 import cons
 from webscrapers.beautifulsoup.main import main
 
 # if running as main programme
 if __name__ == '__main__':
     
+    # set up logging
+    lgr = logging.getLogger()
+    lgr.setLevel(logging.INFO)
+    
     # set number of images constants
     n_images = cons.n_images
-
+    
     # run main function
-    main(search = 'cat', n_images = n_images, home_url = cons.home_url, output_dir = cons.train_fdir)
-    main(search = 'dog', n_images = n_images, home_url = cons.home_url, output_dir = cons.train_fdir)
+    main(search='cat', n_images=n_images, home_url=cons.home_url, output_dir=cons.train_fdir)
+    main(search='dog', n_images=n_images, home_url=cons.home_url, output_dir=cons.train_fdir)
