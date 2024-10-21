c60a8cabe2b2c4c156b8496f9d5c79541e3b7d40
Oisin <oisin.leonard@gmail.com>
2024-10-21 20:50:56 +0100

    Running script as main. Setting up logging. Loading download comp data from webscrapers subdirectory

--- 01_prg_kaggle_data.py
+++ 01_prg_kaggle_data.py
@@ -1,15 +1,23 @@
 # load relevant libraries
 import os
 import sys
+import logging
 
 # load custom modules
 import cons
-from download_comp_data import download_comp_data
-     
-# download competition data
-download_comp_data(comp_name = cons.comp_name,
-                   data_dir = cons.data_fdir,
-                   download_data = cons.download_data, 
-                   unzip_data = cons.unzip_data, 
-                   del_zip = cons.del_zip
-                   )
+from webscrapers.download_comp_data import download_comp_data
+
+if __name__ == "__main__":
+    
+    # set up logging
+    lgr = logging.getLogger()
+    lgr.setLevel(logging.INFO)
+    
+    # download competition data
+    download_comp_data(
+        comp_name=cons.comp_name,
+        data_dir=cons.data_fdir,
+        download_data=cons.download_data, 
+        unzip_data=cons.unzip_data, 
+        del_zip=cons.del_zip
+        )
