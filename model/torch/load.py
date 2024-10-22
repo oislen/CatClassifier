a93b53fd81f7ee8d2290c15638e33a01bee0356f
Oisin <oisin.leonard@gmail.com>
2024-10-22 11:50:28 +0100

    Added default parameter for load_weights=False

--- model/torch/load.py
+++ model/torch/load.py
@@ -1,7 +1,7 @@
 import torch
 
-def load(model, input_fpath):
-    model.load_state_dict(torch.load(input_fpath))
+def load(model, input_fpath, weights_only=False):
+    model.load_state_dict(torch.load(input_fpath, weights_only=weights_only))
     model.eval()
     msg = f'Loaded from {input_fpath}'
     return msg
