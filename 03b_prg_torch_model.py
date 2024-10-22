20ac5a80a09eb5b0fe7545b53920f1c9cd5db48c
Oisin <oisin.leonard@gmail.com>
2024-10-22 21:10:17 +0100

    Added torch model logging

--- 03b_prg_torch_model.py
+++ 03b_prg_torch_model.py
@@ -50,6 +50,7 @@ if __name__ == "__main__":
     lgr = logging.getLogger()
     lgr.setLevel(logging.INFO)
     
+    logging.info("Generating dataframe of images...")
     # create a dataframe of filenames and categories
     filenames = os.listdir(cons.train_fdir)
     categories = [1 if filename.split('.')[0] == 'dog' else 0 for filename in filenames]
@@ -61,11 +62,13 @@ if __name__ == "__main__":
     df['source'] = df['filename'].str.contains(pat='[cat|dog].[0-9]+\.jpg', regex=True).map({True:'kaggle', False:'webscraper'})
     df["filepath"] = cons.train_fdir + '/' + df['filename']
     
+    logging.info("Plot sample image...")
     # random image plot
     sample = random.choice(filenames)
     image = load_img(os.path.join(cons.train_fdir, sample))
     plot_image(image, output_fpath=cons.torch_random_image_fpath)
     
+    logging.info("Split into training, validation and test dataset...")
     # prepare data
     random_state = 42
     validate_df = df[df['source'] == 'kaggle'].sample(n=int(5000 * frac), random_state=random_state)
@@ -73,22 +76,23 @@ if __name__ == "__main__":
     train_df = train_df.reset_index(drop=True)
     validate_df = validate_df.reset_index(drop=True)
     
+    logging.info("Creating training and validation data loaders...")
     # set data constants
     total_train = train_df.shape[0]
     total_validate = validate_df.shape[0]
-    
     # set train datagen
     train_dataset = CustomDataset(train_df, transforms=torch_transforms, mode='train')
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    
     # set validation datagen
     validation_dataset = CustomDataset(train_df, transforms=torch_transforms, mode='train')
     validation_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=True)
     
+    logging.info("Plot example data loader images...")
     # datagen example
     example_generator = [(image.detach().numpy(), None) for images, labels in train_loader for image in images]
     plot_generator(generator=example_generator, mode='torch', output_fpath=cons.torch_generator_plot_fpath)
     
+    logging.info("Initiate torch model...")
     # initiate cnn architecture
     model = VGG16_pretrained(num_classes=2).to(device)
     criterion = nn.CrossEntropyLoss()
@@ -96,19 +100,24 @@ if __name__ == "__main__":
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, threshold=0.0001, threshold_mode='abs')
     early_stopper = EarlyStopper(patience=3, min_delta=0.3)
     
+    logging.info("Fit torch model...")
     # fit torch model
     model.fit(device=device, criterion=criterion, optimizer=optimizer, train_dataloader=train_loader, num_epochs=num_epochs, scheduler=scheduler, valid_dataLoader=validation_loader, early_stopper=early_stopper)
     
+    logging.info("Plot model fit results...")
     # plot model fits
     plot_model_fit(model_fit=model.model_fit, output_fdir=cons.torch_report_fdir)
     
+    logging.info("Save fitted torch model to disk...")
     # save model
     model.save(output_fpath=cons.torch_model_pt_fpath)
     
+    logging.info("Load fitted torch model from disk...")
     # load model
     model = VGG16_pretrained(num_classes=2).to(device)
     model.load(input_fpath=cons.torch_model_pt_fpath)
     
+    logging.info("Generate test dataset...")
     # prepare test data
     test_filenames = os.listdir(cons.test_fdir)
     test_df = pd.DataFrame({'filename': test_filenames})
@@ -117,18 +126,22 @@ if __name__ == "__main__":
     test_df = test_df.set_index('idx').sort_index()
     nb_samples = test_df.shape[0]
     
+    logging.info("Create test dataloader...")
     # set train datagen
     test_dataset = CustomDataset(test_df, transforms=torch_transforms, mode='test')
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
     
+    logging.info("Generate test set predictions...")
     # make test set predictions
     predict = model.predict(test_loader, device)
     test_df['category'] = np.argmax(predict, axis=-1)
     test_df["category"] = test_df["category"].replace(category_mapper) 
     
+    logging.info("Plot example test set predictions...")
     # plot random sample predictions
     plot_preds(data=test_df, cons=cons, output_fpath=cons.torch_pred_images_fpath)
     
+    logging.info("Generate a sample submission file for kaggle...")
     # make submission
     submission_df = test_df.copy()
     submission_df['id'] = submission_df['filename'].str.split('.').str[0]
