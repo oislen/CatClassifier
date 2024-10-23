dff2afdeb44accf9123c1fd793d30934af3f9e8d
Oisin <oisin.leonard@gmail.com>
2024-10-23 19:27:54 +0100

    Created callable main python function with docstring

--- webscrapers/prg_scrape_imgs.py
+++ webscrapers/prg_scrape_imgs.py
@@ -2,6 +2,51 @@ import logging
 import cons
 from webscrapers.download_comp_data import download_comp_data
 from webscrapers.beautifulsoup.webscraper import webscraper
+from beartype import beartype
+
+@beartype
+def scrape_imags(
+    run_download_comp_data:bool=False, 
+    run_webscraper:bool=False
+    ):
+    """Programme for running Kaggle comp data download and image web scrapers
+
+    Parameters
+    ----------
+    run_download_comp_data : bool
+        Whether to run the download Kaggle competition data, default is False
+    run_webscraper : bool
+        Whether to run the image webscraper
+
+    Returns
+    -------
+    """
+    if run_download_comp_data:
+        logging.info('Downloading kaggle data ...')
+        # download competition data
+        download_comp_data(
+            comp_name=cons.comp_name,
+            data_dir=cons.data_fdir,
+            download_data=cons.download_data, 
+            unzip_data=cons.unzip_data, 
+            del_zip=cons.del_zip
+            )
+    if run_webscraper:
+        logging.info('Running image webscraper ...')
+        # run cat webscraper
+        webscraper(
+            search='cat', 
+            n_images=cons.n_images, 
+            home_url=cons.home_url, 
+            output_dir=cons.train_fdir
+            )
+        # run dog webscraper
+        webscraper(
+            search='dog', 
+            n_images=cons.n_images, 
+            home_url=cons.home_url, 
+            output_dir=cons.train_fdir
+            )
 
 # if running as main programme
 if __name__ == '__main__':
@@ -10,17 +55,8 @@ if __name__ == '__main__':
     lgr = logging.getLogger()
     lgr.setLevel(logging.INFO)
 
-    logging.info('Downloading kaggle data ...')
-    # download competition data
-    download_comp_data(
-        comp_name=cons.comp_name,
-        data_dir=cons.data_fdir,
-        download_data=cons.download_data, 
-        unzip_data=cons.unzip_data, 
-        del_zip=cons.del_zip
+    # run the scrape images programme
+    scrape_imags(
+        run_download_comp_data=True, 
+        run_webscraper=True
         )
-
-    logging.info('Running image webscraper ...')
-    # run main function
-    webscraper(search='cat', n_images=cons.n_images, home_url=cons.home_url, output_dir=cons.train_fdir)
-    webscraper(search='dog', n_images=cons.n_images, home_url=cons.home_url, output_dir=cons.train_fdir)
