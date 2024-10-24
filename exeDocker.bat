765bf3b864a99fd56a18e707fad5af892d6acc1d
Oisin <oisin.leonard@gmail.com>
2024-10-24 21:09:32 +0100

    Removed ubuntu dir setting

--- exeDocker.bat
+++ exeDocker.bat
@@ -13,7 +13,6 @@ docker image rm -f %DOCKER_IMAGE%
 call docker build --no-cache -t %DOCKER_IMAGE% . --build-arg GIT_BRANCH=%GIT_BRANCH%
 
 :: run docker container
-SET UBUNTU_DIR=/home/ubuntu
 call docker run --name %DOCKER_CONTAINER_NAME% --shm-size=512m --publish 8888:8888 --volume E:\GitHub\Cat-Classifier\.cred:/home/ubuntu/Cat-Classifier/.cred  --volume E:\GitHub\Cat-Classifier\report:/home/ubuntu/Cat-Classifier/report -it %DOCKER_IMAGE%
 
 :: useful docker commands
