a5680b97584dc3236776b68d84add78e92c467b4
Oisin <oisin.leonard@gmail.com>
2024-10-24 20:28:45 +0100

    Added logic for pulling specific dev tag. Added container name

--- exeDocker.bat
+++ exeDocker.bat
@@ -3,16 +3,18 @@ SET DOCKER_USER=oislen
 SET DOCKER_REPO=cat-classifier
 SET DOCKER_TAG=latest
 SET DOCKER_IMAGE=%DOCKER_USER%/%DOCKER_REPO%:%DOCKER_TAG%
+SET DOCKER_CONTAINER_NAME=cc
+SET GIT_BRANCH=v0.0.0
 
 :: remove existing docker containers and images
 docker rm -f %DOCKER_IMAGE%
 
 :: build docker image
-call docker build --no-cache -t %DOCKER_IMAGE% . 
+call docker build --no-cache -t %DOCKER_IMAGE% . --build-arg GIT_BRANCH=%GIT_BRANCH%
 
 :: run docker container
 SET UBUNTU_DIR=/home/ubuntu
-call docker run --shm-size=512m --publish 8888:8888 --volume E:\GitHub\Cat-Classifier\.cred:/home/ubuntu/Cat-Classifier/.cred  --volume E:\GitHub\Cat-Classifier\report:/home/ubuntu/Cat-Classifier/report -it %DOCKER_IMAGE%
+call docker run --name %DOCKER_CONTAINER_NAME% --shm-size=512m --publish 8888:8888 --volume E:\GitHub\Cat-Classifier\.cred:/home/ubuntu/Cat-Classifier/.cred  --volume E:\GitHub\Cat-Classifier\report:/home/ubuntu/Cat-Classifier/report -it %DOCKER_IMAGE%
 
 :: useful docker commands
 :: docker images
