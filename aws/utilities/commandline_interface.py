b35db1539cafab709f736d95c9d9600889b45ae0
Oisin <oisin.leonard@gmail.com>
2024-10-31 21:04:25 +0000

    Added arguments for describe and isFleet

--- aws/utilities/commandline_interface.py
+++ aws/utilities/commandline_interface.py
@@ -15,8 +15,10 @@ def commandline_interface() -> dict:
     # define argument parser object
     parser = argparse.ArgumentParser(description="Execute Random TeleCom Data Programme.")
     # add input arguments
-    parser.add_argument("--launch", action=argparse.BooleanOptionalAction, dest="launch", type=bool, default=False, help="Boolean, whether to launch a new ec2 instance",)
-    parser.add_argument("--terminate", action=argparse.BooleanOptionalAction, dest="terminate", type=bool, default=False, help="Boolean, whether to terminate all running ec2 instances",)
+    parser.add_argument("--launch", action=argparse.BooleanOptionalAction, dest="launch", type=bool, default=False, help="Boolean, whether to launch a new ec2 instance / fleets",)
+    parser.add_argument("--terminate", action=argparse.BooleanOptionalAction, dest="terminate", type=bool, default=False, help="Boolean, whether to terminate all running ec2 instances / fleets",)
+    parser.add_argument("--describe", action=argparse.BooleanOptionalAction, dest="describe", type=bool, default=False, help="Boolean, whether to describe all running ec2 instances / fleets",)
+    parser.add_argument("--isFleet", action=argparse.BooleanOptionalAction, dest="isFleet", type=bool, default=False, help="Boolean, whether to ec2 type is a Fleet, otherwise a single instance",)
     # create an output dictionary to hold the results
     input_params_dict = {}
     # extract input arguments
@@ -24,4 +26,6 @@ def commandline_interface() -> dict:
     # map input arguments into output dictionary
     input_params_dict["launch"] = args.launch
     input_params_dict["terminate"] = args.terminate
+    input_params_dict["describe"] = args.describe
+    input_params_dict["isFleet"] = args.isFleet
     return input_params_dict
