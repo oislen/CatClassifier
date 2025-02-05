8ea8ae4fd94ca7e300997d5dfd52bc9097057388
Oisin <oisin.leonard@gmail.com>
2025-02-05 18:29:02 +0000

    Replaced index with i enumerate over

--- model/utilities/plot_preds.py
+++ model/utilities/plot_preds.py
@@ -6,11 +6,11 @@ def plot_preds(data, cons, output_fpath = None):
     """"""
     sample_test = data.head(18)
     plt.figure(figsize=(12, 24))
-    for index, row in sample_test.iterrows():
+    for id, (index, row) in enumerate(sample_test.iterrows()):
         filename = row['filename']
         category = row['category']
         img = load_img(os.path.join(cons.test_fdir, filename), target_size=cons.IMAGE_SIZE)
-        plt.subplot(6, 3, index+1)
+        plt.subplot(6, 3, id+1)
         plt.imshow(img)
         #plt.title(category)
         plt.xlabel(filename + '(' + "{}".format(category) + ')' )
