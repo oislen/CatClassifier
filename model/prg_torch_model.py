10598aacdf8b0eaf5408a06f85affb659512c15c
Oisin <oisin.leonard@gmail.com>
2025-02-05 20:54:38 +0000

    Logging process runtimes

--- model/prg_torch_model.py
+++ model/prg_torch_model.py
@@ -27,6 +27,7 @@ from model.utilities.plot_model import plot_model_fit
 from model.utilities.plot_preds import plot_preds
 from model.utilities.plot_image import plot_image
 from model.utilities.plot_generator import plot_generator
+from model.utilities.TimeIt import TimeIt
 
 # hyper-parameters
 num_epochs = 3  if cons.FAST_RUN else 10
@@ -50,6 +51,7 @@ if __name__ == "__main__":
     # set up logging
     lgr = logging.getLogger()
     lgr.setLevel(logging.INFO)
+    timeLogger = TimeIt()
     
     logging.info("Generating dataframe of images...")
     # create a dataframe of filenames and categories
@@ -64,12 +66,14 @@ if __name__ == "__main__":
     df["filepath"] = cons.train_fdir + '/' + df['filename']
     df["ndims"] = df['filepath'].apply(lambda x: len(np.array(Image.open(x)).shape))
     df = df.loc[df["ndims"] == 3, :].copy()
+    timeLogger.logTime(parentKey="DataPrep", subKey="TrainDataLoad")
     
     logging.info("Plot sample image...")
     # random image plot
     sample = random.choice(filenames)
     image = load_img(os.path.join(cons.train_fdir, sample))
     plot_image(image, output_fpath=cons.torch_random_image_fpath)
+    timeLogger.logTime(parentKey="Plots", subKey="SampleImage")
     
     logging.info("Split into training, validation and test dataset...")
     # prepare data
@@ -78,6 +82,7 @@ if __name__ == "__main__":
     train_df = df[~df.index.isin(validate_df.index)]
     train_df = train_df.reset_index(drop=True)
     validate_df = validate_df.reset_index(drop=True)
+    timeLogger.logTime(parentKey="DataPrep", subKey="TrainValidationSplit")
     
     logging.info("Creating training and validation data loaders...")
     # set data constants
@@ -89,11 +94,13 @@ if __name__ == "__main__":
     # set validation datagen
     validation_dataset = CustomDataset(train_df, transforms=torch_transforms, mode='train')
     validation_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=True, num_workers=cons.num_workers, pin_memory=True)
+    timeLogger.logTime(parentKey="DataPrep", subKey="TrainValidationDataLoaders")
     
     logging.info("Plot example data loader images...")
     # datagen example
     example_generator = [(image.detach().numpy(), None) for images, labels in train_loader for image in images]
     plot_generator(generator=example_generator, mode='torch', output_fpath=cons.torch_generator_plot_fpath)
+    timeLogger.logTime(parentKey="Plots", subKey="DataLoader")
     
     logging.info("Initiate torch model...")
     # initiate cnn architecture
@@ -102,23 +109,28 @@ if __name__ == "__main__":
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, threshold=0.0001, threshold_mode='abs')
     early_stopper = EarlyStopper(patience=3, min_delta=0.3)
+    timeLogger.logTime(parentKey="Modelling", subKey="InitiateTorchModel")
     
     logging.info("Fit torch model...")
     # fit torch model
     model.fit(device=device, criterion=criterion, optimizer=optimizer, train_dataloader=train_loader, num_epochs=num_epochs, scheduler=scheduler, valid_dataLoader=validation_loader, early_stopper=early_stopper)
+    timeLogger.logTime(parentKey="Modelling", subKey="Fit")
     
     logging.info("Plot model fit results...")
     # plot model fits
     plot_model_fit(model_fit=model.model_fit, output_fdir=cons.torch_report_fdir)
+    timeLogger.logTime(parentKey="Plots", subKey="ModelFit")
     
     logging.info("Save fitted torch model to disk...")
     # save model
     model.save(output_fpath=cons.torch_model_pt_fpath)
+    timeLogger.logTime(parentKey="ModelSerialisation", subKey="Write")
     
     logging.info("Load fitted torch model from disk...")
     # load model
     model = VGG16_pretrained(num_classes=2).to(device)
     model.load(input_fpath=cons.torch_model_pt_fpath)
+    timeLogger.logTime(parentKey="ModelSerialisation", subKey="Load")
     
     logging.info("Generate test dataset...")
     # prepare test data
@@ -128,25 +140,30 @@ if __name__ == "__main__":
     test_df["idx"] = test_df['filename'].str.extract(pat='([0-9]+)').astype(int)
     test_df = test_df.set_index('idx').sort_index()
     nb_samples = test_df.shape[0]
+    timeLogger.logTime(parentKey="TestSet", subKey="RawLoad")
     
     logging.info("Create test dataloader...")
     # set train datagen
     test_dataset = CustomDataset(test_df, transforms=torch_transforms, mode='test')
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=cons.num_workers, pin_memory=True)
+    timeLogger.logTime(parentKey="TestSet", subKey="DataLoader")
     
     logging.info("Generate test set predictions...")
     # make test set predictions
     predict = model.predict(test_loader, device)
     test_df['category'] = np.argmax(predict, axis=-1)
-    test_df["category"] = test_df["category"].replace(category_mapper) 
+    test_df["category"] = test_df["category"].replace(category_mapper)
+    timeLogger.logTime(parentKey="TestSet", subKey="ModelPredictions")
     
     logging.info("Plot example test set predictions...")
     # plot random sample predictions
     plot_preds(data=test_df, cons=cons, output_fpath=cons.torch_pred_images_fpath)
+    timeLogger.logTime(parentKey="Plots", subKey="TestSetPredictions")
     
     logging.info("Generate a sample submission file for kaggle...")
     # make submission
     submission_df = test_df.copy()
     submission_df['id'] = submission_df['filename'].str.split('.').str[0]
-    submission_df['label'] = submission_df['category'].replace({ 'dog': 1, 'cat': 0 })
+    submission_df['label'] = submission_df['category'].replace(category_mapper)
     submission_df.to_csv(cons.submission_csv_fpath, index=False)
+    timeLogger.logTime(parentKey="TestSet", subKey="SubmissionFile")
