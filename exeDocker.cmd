8b9b3dd1ce910f7e1c53eb04ec6b6653034af207
Oisin <oisin.leonard@gmail.com>
2025-02-05 21:02:35 +0000

    Removed git branch logic

--- exeDocker.cmd
+++ exeDocker.cmd
@@ -4,13 +4,12 @@ SET DOCKER_REPO=cat-classifier
 SET DOCKER_TAG=latest
 SET DOCKER_IMAGE=%DOCKER_USER%/%DOCKER_REPO%:%DOCKER_TAG%
 SET DOCKER_CONTAINER_NAME=cc
-SET GIT_BRANCH=v0.0.0
 
 :: remove existing docker containers and images
 docker image rm -f %DOCKER_IMAGE%
 
 :: build docker image
-call docker build --no-cache -t %DOCKER_IMAGE% . --build-arg GIT_BRANCH=%GIT_BRANCH%
+call docker build --no-cache -t %DOCKER_IMAGE% .
 
 :: run docker container
 call docker run --name %DOCKER_CONTAINER_NAME% --shm-size=512m --publish 8888:8888 --volume E:\GitHub\CatClassifier\.creds:/home/ubuntu/CatClassifier/.creds  --volume E:\GitHub\CatClassifier\report:/home/ubuntu/CatClassifier/report -it %DOCKER_IMAGE%
