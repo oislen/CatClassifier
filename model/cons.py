df80ac0ee783a5dc7f94b782ce2eebb93146ccad
Oisin <oisin.leonard@gmail.com>
2025-02-07 13:42:32 +0000

    Storing and standardising shared model constants between keras and torch

--- model/cons.py
+++ model/cons.py
@@ -61,7 +61,11 @@ IMAGE_HEIGHT=128
 IMAGE_SIZE=(IMAGE_WIDTH, IMAGE_HEIGHT)
 IMAGE_CHANNELS=3
 input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)
-batch_size=16
+batch_size=64
+learning_rate=0.001
+min_epochs=3
+max_epochs=10
+category_mapper={0: 'cat', 1: 'dog'}
 
 # data generator constants
 rescale = 1./255
