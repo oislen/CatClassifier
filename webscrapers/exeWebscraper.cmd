2ac330e8667c0c9b4c6c2a5094f9efaa69e067ed
Oisin <oisin.leonard@gmail.com>
2025-02-07 17:25:11 +0000

    Executing webscraper in python debug mode

--- webscrapers/exeWebscraper.cmd
+++ webscrapers/exeWebscraper.cmd
@@ -1,1 +1,1 @@
-call python prg_scrape_imgs.py --run_download_comp_data --run_webscraper
+call python -m pdb prg_scrape_imgs.py --run_download_comp_data --run_webscraper
--- webscrapers/exeWebscraper.sh
+++ webscrapers/exeWebscraper.sh
@@ -1,1 +1,1 @@
-python prg_scrape_imgs.py --run_download_comp_data --run_webscraper
+python -m pdb prg_scrape_imgs.py --run_download_comp_data --run_webscraper
