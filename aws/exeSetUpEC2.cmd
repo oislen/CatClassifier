6af99e16aecc22609b4fa7869cc77f14d0379488
Oisin <oisin.leonard@gmail.com>
2025-02-13 08:54:37 +0000

    Replaced ec2-user with ubuntu. Updated removed sed -i replace and bash linux_docker_setup

--- aws/exeSetUpEC2.cmd
+++ aws/exeSetUpEC2.cmd
@@ -1,5 +1,6 @@
 :: set EC2 login info
-SET EC2_USER=ec2-user
+::SET EC2_USER=ec2-user
+SET EC2_USER=ubuntu
 SET EC2_PEM_FPATH="C:\Users\oisin\.aws\kaggle.pem"
 SET EC2_CREDS_FDIR=E:\GitHub\CatClassifier\.creds
 SET EC2_SETUP_FPATH=E:\GitHub\CatClassifier\aws\linux_docker_setup.sh
@@ -17,5 +18,6 @@ call scp -i %EC2_PEM_FPATH% -r %EC2_CREDS_FDIR% %EC2_USER%@%EC2_DNS%:~/.
 call scp -i %EC2_PEM_FPATH% %EC2_SETUP_FPATH% %EC2_USER%@%EC2_DNS%:~/linux_docker_setup.sh
 call scp -i %EC2_PEM_FPATH% %EC2_EXTRACT_FPATH% %EC2_USER%@%EC2_DNS%:~/docker_extract_data.sh
 :: ssh to EC2 and run linux setp
-call ssh -v -i %EC2_PEM_FPATH% %EC2_USER%@%EC2_DNS% "sed -i 's/\r$//' ~/linux_docker_setup.sh; bash ~/linux_docker_setup.sh"
+call ssh -v -i %EC2_PEM_FPATH% %EC2_USER%@%EC2_DNS%
+::call ssh -v -i %EC2_PEM_FPATH% %EC2_USER%@%EC2_DNS% "sed -i 's/\r$//' ~/linux_docker_setup.sh; bash ~/linux_docker_setup.sh"
 ENDLOCAL
