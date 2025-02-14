d3b742aafc58bfd68ad0d795ffbc8f51cc6783cb
Oisin <oisin.leonard@gmail.com>
2025-02-14 15:35:42 +0000

    Applying deep copy

--- model/arch/load_image_v2.py
+++ model/arch/load_image_v2.py
@@ -1,6 +1,8 @@
 
 import pandas as pd
+import numpy as np
 from PIL import Image
+from copy import deepcopy
 
 def load_image_v2(image_fpaths):
     """
@@ -8,7 +10,7 @@ def load_image_v2(image_fpaths):
     images = []
     for image_fpath in image_fpaths:
         temp = Image.open(image_fpath)
-        keep = temp.copy()
+        keep = deepcopy(temp)
         images.append(keep)
         temp.close()
     return pd.Series(images)
