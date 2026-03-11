cda5e39dbe8afb094bc031b830cf5c6d5ec9430d
Oisin <oisin.leonard@gmail.com>
2026-03-11 14:20:53 +0000

    Updated uv environment

--- config/uv/catclass.cmd
+++ config/uv/catclass.cmd
@@ -2,7 +2,4 @@
 :: call powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
 :: uv init CatClassifier
 call uv add -r ..\..\requirements.txt --link-mode=copy
-call uv add --index https://download.pytorch.org/whl/cu121 torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --link-mode=copy
-:: call uv add torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --extra-index-url https://download.pytorch.org/whl/cu121
-:: call uv add torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu121
-:: call uv pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --extra-index-url https://download.pytorch.org/whl/cu121 --link-mode=copy
+call uv add --index https://download.pytorch.org/whl/cu129 torch==2.8.0 torchvision==0.23.0 torchaudio==2.8.0 --link-mode=copy
