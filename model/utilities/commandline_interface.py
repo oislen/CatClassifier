c6db9bb727dbd8dea72f09b30dabc35c5e497f95
Oisin <oisin.leonard@gmail.com>
2026-03-16 09:08:29 +0000

    Added model_id to commandline interface

--- model/utilities/commandline_interface.py
+++ model/utilities/commandline_interface.py
@@ -16,6 +16,7 @@ def commandline_interface():
     # add input arguments
     parser.add_argument("--run_model_training", action=argparse.BooleanOptionalAction, dest="run_model_training", type=bool, default=False, help="Boolean, whether to run the model training pipeline, default is False",)
     parser.add_argument("--run_testset_prediction", action=argparse.BooleanOptionalAction, dest="run_testset_prediction", type=bool, default=False, help="Boolean, whether to run predictions on the test set, default is False",)
+    parser.add_argument("--model_id", dest="model_id", type=str, default="VGG16_pretrained", choices=['AlexNet8_pretrained', 'VGG16_pretrained', 'ResNet50_pretrained'], help="String, id of the model architecture to use, default is VGG16_pretrained",)
     # create an output dictionary to hold the results
     input_params_dict = {}
     # extract input arguments
@@ -23,4 +24,5 @@ def commandline_interface():
     # map input arguments into output dictionary
     input_params_dict["run_model_training"] = args.run_model_training
     input_params_dict["run_testset_prediction"] = args.run_testset_prediction
+    input_params_dict["model_id"] = args.model_id
     return input_params_dict
