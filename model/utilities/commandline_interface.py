d6c570eabd78ae5babb002fa4f133695dfbd09eb
Oisin <oisin.leonard@gmail.com>
2026-03-19 15:24:11 +0000

    Added device as commandline parameter, with default cuda if available otherwise cpu

--- model/utilities/commandline_interface.py
+++ model/utilities/commandline_interface.py
@@ -1,4 +1,9 @@
 import argparse
+import torch
+import cons
+
+# device configuration
+device = torch.device('cuda' if torch.cuda.is_available() and cons.check_gpu else 'cpu')
 
 def commandline_interface():
     """A commandline interface for parsing input parameters with
@@ -17,6 +22,7 @@ def commandline_interface():
     parser.add_argument("--run_model_training", action=argparse.BooleanOptionalAction, dest="run_model_training", type=bool, default=False, help="Boolean, whether to run the model training pipeline, default is False",)
     parser.add_argument("--run_testset_prediction", action=argparse.BooleanOptionalAction, dest="run_testset_prediction", type=bool, default=False, help="Boolean, whether to run predictions on the test set, default is False",)
     parser.add_argument("--model_id", dest="model_id", type=str, default="VGG16_pretrained", choices=['AlexNet8_pretrained', 'VGG16_pretrained', 'ResNet50_pretrained'], help="String, id of the model architecture to use, default is VGG16_pretrained",)
+    parser.add_argument("--device", dest="device", type=str, default=device, choices=['cpu', 'cuda'], help="String, device to run the model on, default is cuda if available otherwise cpu",)
     # create an output dictionary to hold the results
     input_params_dict = {}
     # extract input arguments
@@ -25,4 +31,5 @@ def commandline_interface():
     input_params_dict["run_model_training"] = args.run_model_training
     input_params_dict["run_testset_prediction"] = args.run_testset_prediction
     input_params_dict["model_id"] = args.model_id
+    input_params_dict["device"] = args.device
     return input_params_dict
