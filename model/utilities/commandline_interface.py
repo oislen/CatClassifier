d02c827ef823d11133123664d0873425b1ef6230
Oisin <oisin.leonard@gmail.com>
2026-03-19 21:40:07 +0000

    converted device into device_type

--- model/utilities/commandline_interface.py
+++ model/utilities/commandline_interface.py
@@ -18,7 +18,7 @@ def commandline_interface():
     parser.add_argument("--run_model_training", action=argparse.BooleanOptionalAction, dest="run_model_training", type=bool, default=False, help="Boolean, whether to run the model training pipeline, default is False",)
     parser.add_argument("--run_testset_prediction", action=argparse.BooleanOptionalAction, dest="run_testset_prediction", type=bool, default=False, help="Boolean, whether to run predictions on the test set, default is False",)
     parser.add_argument("--model_id", dest="model_id", type=str, default="VGG16_pretrained", choices=['AlexNet8_pretrained', 'VGG16_pretrained', 'ResNet50_pretrained'], help="String, id of the model architecture to use, default is VGG16_pretrained",)
-    parser.add_argument("--device", dest="device", type=str, default=cons.device, choices=['cpu', 'cuda'], help="String, device to run the model on, default is cuda if available otherwise cpu",)
+    parser.add_argument("--device_type", dest="device_type", type=str, default=cons.device_type, choices=['cpu', 'cuda'], help="String, device type to run the model on, default is cuda if available otherwise cpu",)
     # create an output dictionary to hold the results
     input_params_dict = {}
     # extract input arguments
@@ -27,5 +27,5 @@ def commandline_interface():
     input_params_dict["run_model_training"] = args.run_model_training
     input_params_dict["run_testset_prediction"] = args.run_testset_prediction
     input_params_dict["model_id"] = args.model_id
-    input_params_dict["device"] = args.device
+    input_params_dict["device_type"] = args.device_type
     return input_params_dict
