4b400257734f1bf280c4dea210a67b6433b4199b
Oisin <oisin.leonard@gmail.com>
2026-03-20 12:04:13 +0000

    Updated --device_type to gpu

--- model/exeTorchModel.cmd
+++ model/exeTorchModel.cmd
@@ -1,4 +1,4 @@
 call SET PARAM_CHECK_GPU=True
 :: call uv run python prg_torch_model.py --run_model_training --run_testset_prediction --model_id AlexNet8_pretrained
-call uv run python prg_torch_model.py --run_model_training --run_testset_prediction --model_id VGG16_pretrained --device_type cpu
+call uv run python prg_torch_model.py --run_model_training --run_testset_prediction --model_id VGG16_pretrained --device_type gpu
 :: call uv run python prg_torch_model.py --run_model_training --run_testset_prediction --model_id ResNet50_pretrained
